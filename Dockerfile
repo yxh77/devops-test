a0e196d33d62ccf342f93b28f1f61241f27697ad
yxh77 <2601074046@qq.com>
2022-12-14 11:08:36 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,7 @@ WORKDIR /app
 
 COPY package*.json ./
 
-RUN npm install
-
-RUN npm install express
+RUN npm install express --save
 
 COPY . .
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {readFile} = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
     readFile('./home.html','utf-8',(err,html)=>{
@@ -11,8 +11,8 @@ app.get('/',(req,res)=>{
 
         res.send(html);
     })
-})
+});
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
-})
+});
